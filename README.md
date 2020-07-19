5d5afd61c948ec87aea47120d2a61a56e87a2802
Scott Doxey <neogeek0101@gmail.com>
2020-07-19 15:58:29 -0400

    Added table of contents to README.

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
 # Bash Snippets
 
-## Array
+> A collection of code snippets I found useful while coding in bash.
 
+- [Array](#Array)
+- [Cli Flags](#Cli%20Flags)
+- [If Statements](#If%20Statements)
+
+## Array
 ```bash
 #!/bin/bash
 
 declare -a FRUITS
 
-# Declare array as a space seperated list
+# Declare an array as a space seperated list.
 FRUITS=(grapefruit pineapple avocado)
 
 for FRUIT in "${FRUITS[@]}"
@@ -16,10 +21,46 @@ do
 	echo "${FRUIT}"
 
 done
+
+SENTENCE="This is a sample sentence."
+
+# Split string on spaces.
+WORDS=(${SENTENCE// / })
+
+echo "${SENTENCE}"
+
+for WORD in "${WORDS[@]}"
+do
+
+	echo "${WORD}"
+
+done
+
+```
+
+## Cli Flags
+```bash
+#!/bin/bash
+
+args=("${@// /}")
+
+for arg in "${args[@]}"
+do
+
+    case $arg in
+
+        -v | --version ) echo "Version flag."; exit;;
+        -h | --help ) echo "Help flag."; exit;;
+
+        ?* ) printf "Invalid option %s\n\n" "${arg}"; exit;;
+
+    esac
+
+done
+
 ```
 
 ## If Statements
-
 ```bash
 #!/bin/bash
 
@@ -30,4 +71,5 @@ if [ -z "${EMPTY}" ]; then
     echo "Variable is empty."
 
 fi
+
 ```
--- bin/generate.js
+++ bin/generate.js
@@ -7,20 +7,26 @@ const SNIPPETS_DIR = './snippets';
 
 const SNIPPET_FILE_EXTENSION = /\.sh$/;
 
+const formatTitle = title =>
+    title
+        .replace('_', ' ')
+        .replace(SNIPPET_FILE_EXTENSION, '')
+        .replace(/(^| )[a-z]/g, match => match.toUpperCase());
+
+const formatContent = content => '```bash\n' + content + '\n```';
+
 const snippets = readdirSync(SNIPPETS_DIR)
     .filter(file => file.match(SNIPPET_FILE_EXTENSION))
-    .map(
-        file =>
-            `## ${file
-                .replace('_', ' ')
-                .replace(SNIPPET_FILE_EXTENSION, '')
-                .replace(/(^| )[a-z]/g, match =>
-                    match.toUpperCase()
-                )}\n\n\`\`\`bash\n${readFileSync(
-                join(SNIPPETS_DIR, file),
-                'utf8'
-            )}\`\`\``
-    )
-    .join('\n\n');
+    .map(file => ({
+        file,
+        title: formatTitle(file),
+        content: formatContent(readFileSync(join(SNIPPETS_DIR, file), 'utf8'))
+    }));
 
-process.stdout.write(`# Bash Snippets\n\n${snippets}\n`);
+process.stdout.write(
+    `# Bash Snippets\n\n> A collection of code snippets I found useful while coding in bash.\n\n${snippets
+        .map(({ title }) => `- [${title}](#${title.replace(/ /, '%20')})`)
+        .join('\n')}\n\n${snippets
+        .map(({ title, content }) => `## ${title}\n${content}`)
+        .join('\n\n')}\n`
+);
