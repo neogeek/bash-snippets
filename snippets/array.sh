7a868973798f2a8f420a45692453ee55a82676f6
Scott Doxey <neogeek0101@gmail.com>
2020-07-19 15:58:06 -0400

    Updated array snippet.

--- snippets/array.sh
+++ snippets/array.sh
@@ -2,12 +2,26 @@
 
 declare -a FRUITS
 
-# Declare array as a space seperated list
+# Declare an array as a space seperated list.
 FRUITS=(grapefruit pineapple avocado)
 
 for FRUIT in "${FRUITS[@]}"
 do
 
 	echo "${FRUIT}"
+
+done
+
+SENTENCE="This is a sample sentence."
+
+# Split string on spaces.
+WORDS=("${SENTENCE// / }")
+
+echo "${SENTENCE}"
+
+for WORD in "${WORDS[@]}"
+do
+
+	echo "${WORD}"
 
 done
