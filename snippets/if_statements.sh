f56b0664f482405e23d4a22bc58e3d38d0a73313
Scott Doxey <neogeek0101@gmail.com>
2020-07-20 01:16:11 -0400

    Show how to compare numbers.

--- snippets/if_statements.sh
+++ snippets/if_statements.sh
@@ -7,3 +7,17 @@ if [ -z "${EMPTY}" ]; then
     echo "Variable is empty."
 
 fi
+
+NUMBER=10
+
+if [ "${NUMBER}" -gt 0 ]; then
+
+    echo "Number is greater than 0."
+
+fi
+
+if [ "${NUMBER}" -lt 100 ]; then
+
+    echo "Number is less than 100."
+
+fi
